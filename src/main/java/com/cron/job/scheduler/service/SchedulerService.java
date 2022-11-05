e9e889aa6a1a5c54b537b14b5fdae4e8d0148fc5
Nikhil Kumar Singh <nikhil.kumar@iot83.com>
2022-11-05 21:45:50 +0530

    Code Refactored

--- src/main/java/com/cron/job/scheduler/service/SchedulerService.java
+++ src/main/java/com/cron/job/scheduler/service/SchedulerService.java
@@ -5,8 +5,10 @@ import com.cron.job.scheduler.exception.ValidationException;
 import com.cron.job.scheduler.repository.SchedulerRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +19,10 @@ import java.util.concurrent.ScheduledFuture;
 @Service
 public class SchedulerService {
 
-    @Autowired
-    private TaskScheduler taskScheduler;
-    @Autowired
-    private SchedulerThread schedulerThread;
+    @Bean
+    private TaskScheduler taskScheduler() {
+        return new ConcurrentTaskScheduler();
+    }
     @Autowired
     private SchedulerRepository schedulerRepository;
     Map<String, ScheduledFuture<?>> jobsMap = new HashMap<>();
@@ -28,17 +30,15 @@ public class SchedulerService {
     public String createScheduler(String cronExpression) {
         String id = UUID.randomUUID().toString().replace("-", "");
         Scheduler scheduler = saveSchedulerInDatabase(cronExpression, id);
-        schedulerThread.setTaskDefinition(scheduler);
         log.info("Scheduling task with job id: " + scheduler.getId() + " and cron expression: " + scheduler.getCronExpression());
-        ScheduledFuture<?> scheduledTask = taskScheduler.schedule(schedulerThread, new CronTrigger(cronExpression, TimeZone.getTimeZone(TimeZone.getDefault().getID())));
+        ScheduledFuture<?> scheduledTask = taskScheduler().schedule(()->{try {System.out.println("Scheduler Running : " + new Date());} catch (Exception e) {log.error("Error In Custom Scheduler : " + e.getMessage());}}, new CronTrigger(cronExpression, TimeZone.getTimeZone(TimeZone.getDefault().getID())));
         jobsMap.put(id, scheduledTask);
         return "scheduler created successfully.";
     }
 
     public void restartReportScheduler(Scheduler scheduler) {
         log.info("Scheduling task with job id: " + scheduler.getId() + " and cron expression: " + scheduler.getCronExpression());
-        schedulerThread.setTaskDefinition(scheduler);
-        ScheduledFuture<?> scheduledTask = taskScheduler.schedule(schedulerThread, new CronTrigger(scheduler.getCronExpression(), TimeZone.getTimeZone(TimeZone.getDefault().getID())));
+        ScheduledFuture<?> scheduledTask = taskScheduler().schedule(()->{try {System.out.println("Scheduler Running : " + new Date());} catch (Exception e) {log.error("Error In Custom Scheduler : " + e.getMessage());}}, new CronTrigger(scheduler.getCronExpression(), TimeZone.getTimeZone(TimeZone.getDefault().getID())));
         jobsMap.put(scheduler.getId(), scheduledTask);
     }
 
@@ -68,5 +68,4 @@ public class SchedulerService {
         schedulerRepository.save(scheduler);
         return scheduler;
     }
-
 }
